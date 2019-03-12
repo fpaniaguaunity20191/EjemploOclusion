cc3eb54a75856fb30184e29af431904e6aded4e2
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-03-12 14:01:14 +0100

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float angularSpeed;
+    float x, z;
+
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * z * Time.deltaTime * speed);
+        transform.Rotate(new Vector3(0, x, 0) * Time.deltaTime * angularSpeed);
+    }
+}
